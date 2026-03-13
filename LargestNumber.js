db723fc4bd9f53e8e234c573c43cbbb76b1b1f14
rahulgope45 <rahuleile456@gmail.com>
2026-03-14 00:25:21 +0530

    Find Largest number in array added

---
+++ LargestNumber.js
@@ -0,0 +1,26 @@
+let arr = [12,34,5,6,78]
+
+//Find the Largest number in the array
+
+
+//First using brute method sorting the array in accending order than retuning the last element
+function Largest(){
+  const sorted = arr.sort((a,b) => a-b);
+   console.log(sorted.at(-1))
+}
+
+Largest()
+
+//Method Second Optimized version
+
+function greatest(){
+    let max = arr[0]
+    for(let i =1; i<arr.length;i++){
+        if(arr[i] > max){
+           max = arr[i]
+        }
+    }
+    return max;
+}
+
+console.log(greatest())
