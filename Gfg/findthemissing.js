e608ae0e1488e5aeed5f729b5b378ca6172f380d
rahulgope45 <rahuleile456@gmail.com>
2026-04-14 13:01:06 +0530

    added find the missing number 2

--- Gfg/findthemissing.js
+++ Gfg/findthemissing.js
@@ -1,13 +1,29 @@
 function findThemissing(arr){
+
+    let n = arr.length +1
     let min = Math.min(...arr);
     let max = Math.max(...arr);
 
+    if(min === 1){
+      let expectedSum = (n *(n+1))/2
+      let actualSum = arr.reduce((a,b) => a+b,0);
+      return expectedSum -actualSum
+    }
+
+    if(max === n){
+      let expectedSum = (n *(n+1))/2
+      let actualSum = arr.reduce((a,b) => a+b,0);
+      return expectedSum -actualSum
+    }
+
     let expectedSum = (max - min +1) * (min + max) /2
-    console.log(expectedSum)
+    
     let actualSum = arr.reduce((a,b) => a+b,0)
 
-    console.log(expectedSum -actualSum)
+    return expectedSum -actualSum
 }
 
-findThemissing([10,11,13])
+console.log(findThemissing([10,11,13]))
+console.log(findThemissing([1]))
+console.log(findThemissing([23]))
 
--- Gfg/tempCodeRunnerFile.js
+++ Gfg/tempCodeRunnerFile.js
@@ -1,9 +1,1 @@
-function findThemissing(arr){
-//     let min = Math.min(...arr);
-//     let max = Math.max(...arr);
-
-//     let expectedSum = (max - min +1) * (min + max) /2
-//     let actualSum = 
-// }
-
-// findThemissing([10,11,13])
+console.log(expectedSum)
